1f90b484122071104b8c819a4572177a295a99d1
George Penkov <george.p@wealthberry.com>
2021-08-06 11:32:18 +0300

    Added quiet flag to unzip operation

--- awscli_install.sh
+++ awscli_install.sh
@@ -21,7 +21,7 @@ echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> "$HOME/.aws/credentials
 chmod 600 "$HOME/.aws/credentials"
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 rm awscliv2.zip
 sudo ./aws/install
 rm -rf aws
