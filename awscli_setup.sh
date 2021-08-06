3a00556ab9f0ef1156acaf6094df75e56ef49a63
George Penkov <george.p@wealthberry.com>
2021-08-06 12:15:44 +0300

    Remove AWS Install and do only setup

--- awscli_setup.sh
+++ awscli_setup.sh
@@ -20,9 +20,4 @@ echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> "$HOME/.aws/credentials"
 echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> "$HOME/.aws/credentials"
 chmod 600 "$HOME/.aws/credentials"
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -q awscliv2.zip
-rm awscliv2.zip
-sudo ./aws/install
-rm -rf aws
 aws --version
